773da8158cabeb64102cf3a49dc1e51046643d8a
Blund1 <brllund@ucdavis.edu>
2014-02-22 03:25:31 -0800

    Create element.h

---
+++ element.h
@@ -0,0 +1,16 @@
+#ifndef ELEMENT_H
+#define ELEMENT_H
+
+#include "dict.h"
+class Element
+{
+public:
+  virtual void complete(const Dict &d) = 0;
+  virtual void check(const Dict &d) = 0;
+  virtual void show() const = 0;
+  
+private:
+//NOT SURE IF ANYTHING IS PRIVATE IN THIS CLASS
+};
+
+#endif
